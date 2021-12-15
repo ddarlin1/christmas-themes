132c0c29f8766b036d009a9e41e1d5a0ddfc2218
darling_d <ddarling212@gmail.com>
2021-12-15 18:22:29 -0500

    Added theme and customized CSS

--- index.js
+++ index.js
@@ -2,18 +2,25 @@ const body = document.getElementById("body")
 const greeting = document.getElementById("greeting")
 const christmasBtn = document.getElementById("christmas")
 const snowBtn = document.getElementById("snow")
-
-// Task:
-//- Add the functionality to switch the theme between 'Christmas' and 'snow'.
+const deerBtn = document.getElementById("deer")
 
 christmasBtn.addEventListener("click", function() {
     body.classList.remove("snow");
+    body.classList.remove("deer");
     body.classList.add("christmas");
-    greeting.innerHTML = `🎅 Merry Christmas!`
+    greeting.innerHTML = `🎅 HO HO HO! Merry Christmas! 🎄`
 });
 
 snowBtn.addEventListener("click", function () {
     body.classList.remove("christmas");
+    body.classList.remove("deer");
     body.classList.add("snow");
-    greeting.innerHTML = `Let it SNOW! ☃️`
+    greeting.innerHTML = `❄️ Let it SNOW! ☃️`
+});
+
+deerBtn.addEventListener("click", function () {
+    body.classList.remove("christmas");
+    body.classList.remove("snow");
+    body.classList.add("deer");
+    greeting.innerHTML = `✨ May your day be merry and BRIGHT 🚨`
 });
