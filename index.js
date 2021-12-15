0bb824b5311e7cc9f19bb7140cbc727e92dbe571
darling_d <ddarling212@gmail.com>
2021-12-15 17:41:13 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const body = document.getElementById("body")
+const greeting = document.getElementById("greeting")
+const christmasBtn = document.getElementById("christmas")
+const snowBtn = document.getElementById("snow")
+
+// Task:
+//- Add the functionality to switch the theme between 'Christmas' and 'snow'.
+
+christmasBtn.addEventListener("click", function() {
+    body.classList.remove("snow");
+    body.classList.add("christmas");
+    greeting.innerHTML = `🎅 Merry Christmas!`
+});
+
+snowBtn.addEventListener("click", function () {
+    body.classList.remove("christmas");
+    body.classList.add("snow");
+    greeting.innerHTML = `Let it SNOW! ☃️`
+});
